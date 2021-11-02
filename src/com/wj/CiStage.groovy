39d40ebbb544d0de19bceb0004ef6bfc26b44f6a
Jiang Wu <john.x.wu@ericsson.com>
2021-11-02 19:45:24 +0200

    test Grab

--- src/com/wj/CiStage.groovy
+++ src/com/wj/CiStage.groovy
@@ -1,5 +1,8 @@
 package com.wj
 
+import org.apache.commons.lang3.StringUtils
+
+@Grab(group='org.apache.commons', module='commons-lang3', version='3.12.0')
 class CiStage {
     String stageName
     Closure stageAction
@@ -7,5 +10,6 @@ class CiStage {
     void runStage() {
         //print "start stage:" + stageName
         stageAction.run()
+        println(StringUtils.capitalize("cat"))
     }
 }
