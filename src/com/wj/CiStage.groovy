94243b853a3874feb482a7f0dc1085ae166b5d0a
Jiang Wu <john.x.wu@ericsson.com>
2021-11-02 19:46:50 +0200

    test Grab

--- src/com/wj/CiStage.groovy
+++ src/com/wj/CiStage.groovy
@@ -1,5 +1,6 @@
 package com.wj
 
+import com.cloudbees.groovy.cps.NonCPS
 import org.apache.commons.lang3.StringUtils
 
 @Grab(group='org.apache.commons', module='commons-lang3', version='3.12.0')
@@ -7,6 +8,7 @@ class CiStage {
     String stageName
     Closure stageAction
 
+    @NonCPS
     void runStage() {
         //print "start stage:" + stageName
         stageAction.run()
