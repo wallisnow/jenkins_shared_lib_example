4590586fbaec19047d03bd2ae3f2a816acafc273
Jiang Wu <john.x.wu@ericsson.com>
2021-11-02 18:33:36 +0200

    test

--- src/com/wj/Student.groovy
+++ src/com/wj/Student.groovy
@@ -1,10 +1,6 @@
 package com.wj
 
 class Student {
-    private static String name
-    private static String course
-
-    static String showStudent() {
-        return "name: " + name + " has course: " + course
-    }
+    String name
+    String course
 }
--- vars/initStudent.groovy
+++ vars/initStudent.groovy
@@ -1,6 +1,6 @@
 import com.wj.Student
 
 def call(String name = "foo", String course = "bar") {
-    def st = new Student(name, course)
-    st.showStudent()
+    def st = new Student('name': name, 'course': course)
+    print st.name
 }
