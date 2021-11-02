61feb0a86abf7f93fe4deeaac46192d9137ba3d9
Jiang Wu <john.x.wu@ericsson.com>
2021-11-02 19:25:13 +0200

    test

--- vars/runStage.groovy
+++ vars/runStage.groovy
@@ -3,9 +3,13 @@ import com.wj.CiStage
 def call() {
 
     Closure myClosure = {
-        sh '''
-           echo 'call myClosure ... ...'
-        '''
+        node {
+            script {
+                sh '''
+                   echo 'call myClosure ... ...'
+                '''
+            }
+        }
     }
 
     def stage = new CiStage('stageName': 'stage1', 'stageAction': myClosure)
